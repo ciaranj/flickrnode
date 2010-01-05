6a379718579df138cc7dba8d49390bf012a66d45
ciaranj <ciaranj@gmail.com>
2010-01-05 19:40:51 +0000

    A first stab at reworking the flickr api so that passing the API key is redundant
    Request for comments ;)

--- examples/most_recent_photoset.js
+++ examples/most_recent_photoset.js
@@ -1,14 +1,16 @@
 var sys= require('sys');
 
 require.paths.unshift('lib')
-var flickr= require('flickr');
+var FlickrAPI= require('flickr').FlickrAPI;
 
 var API_KEY="";
-var USER_NAME= ""
+var USER_NAME= "";
 
-flickr.people.findByUsername(USER_NAME,API_KEY).addErrback(fail).addCallback(function(user){
-    flickr.photosets.getList(user.id, API_KEY).addErrback(fail).addCallback(function(photosets){
-        flickr.photosets.getPhotos(photosets.photoset[0].id, API_KEY, ["url_sq", "url_m"]).addErrback(fail).addCallback(function(photoset){
+var flickr= new FlickrAPI(API_KEY);
+
+flickr.people.findByUsername(USER_NAME).addErrback(fail).addCallback(function(user){
+    flickr.photosets.getList(user.id).addErrback(fail).addCallback(function(photosets){
+        flickr.photosets.getPhotos(photosets.photoset[0].id, ["url_sq", "url_m"]).addErrback(fail).addCallback(function(photoset){
             sys.puts("FOO: " + sys.inspect(photoset));
         });
     });
