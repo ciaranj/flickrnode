b3fc5f103e9e143f77070b91db690c1a03f4c623
John Wright <mrjjwright@gmail.com>
2010-03-21 20:24:49 -0600

    getting flickr filesize was zero error

--- test/test_upload.js
+++ test/test_upload.js
@@ -2,9 +2,9 @@ var FlickrAPI= require('../lib/flickr').FlickrAPI;
 var sys= require('sys');
 
 
-var API_KEY= "5a7499da64e7142d77809edb1f202a72"
-var SHARED_SECRET= "f9eef1ff339d9322"
-var AUTHENTICATION_TOKEN= "72157620432370689-1c40342f96135c89"
+var API_KEY= "enter here"
+var SHARED_SECRET= "enter here"
+var AUTHENTICATION_TOKEN= "enter here"
 
 var flickr= new FlickrAPI(API_KEY, SHARED_SECRET);
 flickr.setAuthenticationToken(AUTHENTICATION_TOKEN);
